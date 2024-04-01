bf6351c0ea90d90006e6cb3d60876c1cf5c0030c
jam2982 <126498855+jam2982@users.noreply.github.com>
2024-04-01 10:38:53 -0300

    Create make-menu-clickable.js

---
+++ make-menu-clickable.js
@@ -0,0 +1,8 @@
+	$(document).ready(function(){
+		$('.menu-item-14155').on('click', function() {
+			location.href = 'https://example.com/page';
+    });
+		$('.menu-item-14156').on('click', function() {
+			location.href = 'https://example.com/page2';
+    });
+  }); 
